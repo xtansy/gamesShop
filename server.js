affeea6f082c6c47c834f59500136b8c9601ebc2
Andrew <skid.one@inbox.ru>
2022-06-23 18:12:13 +0300

    fixserver

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+const express = require("express");
+const path = require("path");
 const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router = jsonServer.router("./src/db.json");
@@ -9,6 +11,13 @@ const PORT = process.env.PORT || 3001;
 
 server.use(middlewares);
 server.use(router);
+
+if (process.env.NODE_ENV === "production") {
+    server.use(express.static("build"));
+    server.get("/", (req, res) => {
+        req.sendFile(path.resolve(__dirname, "build", "index.html"));
+    });
+}
 
 server.listen(PORT, () => {
     console.log("Server is running");
